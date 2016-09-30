9d1ce77a7dbde5c0f35920a4a79f4583ab229a54
ddraper <david.draper@alfresco.com>
2016-09-30 18:39:39 +0100

    5: Added basic movie list rendering for fixed data

--- modules/utils/xhr.js
+++ modules/utils/xhr.js
@@ -2,7 +2,18 @@ import apiKey from "../apiKey";
 
 var xhrUtils = {
 
-   getLegoMovies: function() {
+
+   /**
+    * This function can be used to search The Movie DB for movies using a
+    * query supplied in the input object.
+    * 
+    * @param  {object} input Input parameters for the movie search
+    * @param  {string} input.query The query to use to search for movies
+    * @return {Promise} A promise of the search results
+    */
+   getMovies: function(input) {
+      var queryStr = input.query || "*";
+
       var promise = new Promise(function (resolve, reject) {
          var data = JSON.stringify({});
 
@@ -10,14 +21,24 @@ var xhrUtils = {
          xhr.withCredentials = false;
          xhr.addEventListener("readystatechange", function () {
             if (this.readyState === this.DONE) {
-               resolve(this.responseText);
+
+               if (this.responseText)
+               {
+                  var results = JSON.parse(this.responseText);
+                  resolve(results);
+               }
+               else
+               {
+                  reject("No response");
+               }
             }
          });
          xhr.onerror = function() {
             reject(this.statusText);
          };
 
-         xhr.open("GET", "https://api.themoviedb.org/3/search/movie?language=en-US&api_key=" + apiKey.key + "&query=lego");
+         var url = `https://api.themoviedb.org/3/search/movie?language=en-US&api_key=${apiKey.key}&query=${queryStr}`;
+         xhr.open("GET", url);
          xhr.send(data);
       });
       return promise;
