5f33f212493f24b67af65410d901009603b6ee77
ddraper <david.draper@alfresco.com>
2016-09-30 16:48:23 +0100

    3: Added a module for the API key and tested import

---
+++ modules/apiKey.js
@@ -0,0 +1,10 @@
+// In order to be able to make requests to the Movie DB it is necessary to 
+// change the assignment of the MY_API_KEY const below to your own API key
+// To get an API key you need to register with the Movie DB (at https://www.themoviedb.org) 
+// and request an API key from your account page.
+
+const MY_API_KEY = null;
+
+export default {
+   key: MY_API_KEY
+};
