fb827fcd598c0ed22e7ccc1e66b3efc2df10a025
ddraper <david.draper@alfresco.com>
2016-09-30 18:59:20 +0100

    6: Basic page setup

--- entry.js
+++ entry.js
@@ -6,7 +6,7 @@ import xhrUtils from "./modules/utils/xhr";
 import MovieList from "./modules/widgets/MovieList";
 
 // Simple test that the key has been configured, this will be improved...
-var d = document.createElement("div");
+var d = document.createElement("section");
 if (apiKey.key)
 {
    xhrUtils.getMovies({
@@ -25,4 +25,4 @@ else
 {
    d.appendChild(document.createTextNode("No API Key Configured"));
 }
-document.getElementById("BODY").appendChild(d);
+document.getElementById("MAIN").appendChild(d);
